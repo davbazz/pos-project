2ada28aa4dc7882ce0a5d0a0284636352352f20c
David <callmelmao@gmail.com>
2023-10-04 21:07:26 +0100

    cleaned unused func

--- lib/fetchProductToCart.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from "./clientSupabase";
-
-export default async function fetchProductToCart() {
-  const { data: products, error } = await supabase
-    .from("menu_products")
-    .select("product_name, price, size, img_url")
-    .eq("user_id", (await supabase.auth.getUser()).data.user);
-}
