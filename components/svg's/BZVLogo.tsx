d5e522118ede16de77e202b92e606bd3cd616887
David <callmelmao@gmail.com>
2023-10-31 20:59:19 +0000

    logo colors changed

--- components/svg's/BZVLogo.tsx
+++ components/svg's/BZVLogo.tsx
@@ -6,9 +6,9 @@ const font = Exo_2({ subsets: ['latin'] })
 export default function BZVLogo() {
   return (
     <Flex
-      className={` ${font.className} font-bold text-secondary justify-start items-center`}
+      className={` ${font.className} font-bold text-softSecondary justify-start items-center`}
     >
-      <span className="text-primary">Q</span>POS
+      <span className="text-softPrimary">Q</span>POS
     </Flex>
   )
 }
