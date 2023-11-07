9a7b62622115729ef6e45bfffb87768803a37e72
David <callmelmao@gmail.com>
2023-11-07 06:43:50 +0000

    fixed height at NotSupportedMessage component

--- components/molecules/NotSupportedMessage.tsx
+++ components/molecules/NotSupportedMessage.tsx
@@ -5,7 +5,7 @@ import SadFaceIcon from "../svg's/SadFaceIcon"
 export default function NotSupportedMessage() {
   return (
     <aside className="lg:hidden h-screen">
-      <Flex className="flex-col justify-center items-center p-12 gap-2 h-full text-center">
+      <Flex className="flex-col justify-center items-center p-12 gap-2 h-[100svh] text-center">
         <SadFaceIcon />
         <SubText>
           This screen resolution is not supported, please use larger device
